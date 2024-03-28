8116d5bb13e7ce35555873a3dac8b7ad23acc3c5
Yiqiao Chen <vanilla.att@gmail.com>
2024-03-28 13:09:49 -0700

    files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ISA-API',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Your library dependencies
+    ],
+    author='Yiqiao',
+    author_email='vanilla dot att at gamil dot com',
+    description='This library is to combine some strategies under realistic scenarios with information structures and algorithms',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ISA-API/ISA-API',
+)
