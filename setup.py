5c972406a4111ddb03ae68d96647c438e2b4d39d
yiqiaoc11 <30539007+yiqiaoc11@users.noreply.github.com>
2024-03-28 13:26:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=[
         # Your library dependencies
     ],
-    author='Yiqiao',
+    author='Yiqiao11',
     author_email='vanilla.att@gmail.com',
     description='This library is to combine some strategies under realistic scenarios with information structures and algorithms',
     long_description=open('README.md').read(),
