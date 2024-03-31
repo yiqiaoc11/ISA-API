e97159d9fc892c5999eb4e93aeec1af1d3b67ea2
yiqiaoc11 <30539007+yiqiaoc11@users.noreply.github.com>
2024-03-31 14:42:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     ],
     author='Yiqiao11',
     author_email='vanilla.att@gmail.com',
-    description='This library is to combine some strategies under realistic scenarios with information structures and algorithms',
+    description='This library is to combine strategies under realistic scenarios with information structures and algorithms',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/ISA-API/ISA-API',
